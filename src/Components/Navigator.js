1a5b115a9fc73c165a472392ea21708a3bcfc901
Anh Kim Chau <119595804+anh-8922@users.noreply.github.com>
2023-08-15 16:32:44 +0100

    update

--- src/Components/Navigator.js
+++ src/Components/Navigator.js
@@ -6,18 +6,18 @@ import '../Styles/components.css';
 export default function Navigator() {
     
     return(
-        <ul className='navigator-bar' style={{
+        <div style={{
             backgroundColor:'#C7A87E', padding:'1rem 3rem', 
             display:'flex', flexDirection:'row', 
             justifyContent:'flex-end', gap:'1rem',
             borderTopLeftRadius:'0.7rem', borderTopRightRadius:'0.7rem',
             listStyleType:'none'}}>
-            <li><NavLink className="NavItems" to="/">Profile</NavLink></li>
-            <li><NavLink className="NavItems" to='/projects'>Projects</NavLink></li>
-            <li><NavLink className="NavItems" to='/experiences'>Experiences</NavLink></li>
-            <li><NavLink className="NavItems" to='/blog'>Blog</NavLink></li>
-            <li><NavLink className="NavItems" to='/contact'>Contact</NavLink></li>
-        </ul>
+            <NavLink className="NavItems" to="/">Profile</NavLink>
+            <NavLink className="NavItems" to='/projects'>Projects</NavLink>
+            <NavLink className="NavItems" to='/experiences'>Experiences</NavLink>
+            <NavLink className="NavItems" to='/blog'>Blog</NavLink>
+            <NavLink className="NavItems" to='/contact'>Contact</NavLink>
+        </div>
     )
 }
 

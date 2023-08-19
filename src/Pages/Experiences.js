58226e5e39e0e883aae38541a0fd59a31182d4de
Anh Kim Chau <119595804+anh-8922@users.noreply.github.com>
2023-08-19 17:16:38 +0100

    update timeline

--- src/Pages/Experiences.js
+++ src/Pages/Experiences.js
@@ -1,16 +1,16 @@
 import MainLayout from '../Layout/MainLayout';
 import '../Styles/components.css';
-import milestone from '../Assets/milestone.png';
+import milestone from '../Assets/milestone.jpg';
 import { Link } from 'react-router-dom';
 
 export default function Experiences() {
     return(
         <MainLayout>
-            <div style={{display:'flex', flexDirection:'row', margin:'2rem'}}>
+            <div style={{display:'flex', flexDirection:'row', margin:'2rem', justifyContent:'space-between'}}>
                 <div style={{width:'40%'}}>test</div>
                 <div style={{
                     backgroundImage: `url("${milestone}")`, 
-                    width: '50rem', height:'35rem', 
+                    width: '37%', height:'66rem', 
                     backgroundSize:'cover',
                     borderRadius:'0.5rem'}}></div>
             </div>

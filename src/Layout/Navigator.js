d890e7b64d8a97b5f9588980c7b387462aadc7a6
Anh Kim Chau <119595804+anh-8922@users.noreply.github.com>
2023-09-28 10:53:10 +0100

    update social icons

--- src/Layout/Navigator.js
+++ src/Layout/Navigator.js
@@ -6,6 +6,12 @@ import {CgWebsite} from 'react-icons/cg';
 import {LiaNewspaperSolid} from 'react-icons/lia';
 import {ImBlog} from 'react-icons/im';
 import {TbMessageStar} from 'react-icons/tb';
+//import Social Network Icons Images
+import discord1 from '../Assets/social/discord1.png';
+import faceb from '../Assets/social/faceb.png';
+import github1 from  '../Assets/social/github1.png';
+import hotmail from '../Assets/social/hotmail.png';
+import linked from '../Assets/social/linked.png';
 
 export default function Navigator() {
     return(
@@ -13,11 +19,30 @@ export default function Navigator() {
             <div className="profile-img"></div>
             <p>My name is Anh and I am a Web Developer</p>
             <ul className="social-content">
-                <li><Link></Link></li>
-                <li><Link></Link></li>
-                <li><Link></Link></li>
-                <li><Link></Link></li>
-                <li><Link></Link></li>
+                <li>
+                    <Link to='https://discordapp.com/users/1047844500418080778' target='_blank'>
+                        <img className="social-icons" src={discord1} alt="discord"/>
+                    </Link>
+                </li>
+                <li>
+                    <Link to='https://www.facebook.com/hafsa.chau' target="_blank">
+                        <img className="social-icons" src={faceb} alt="facebook"/>
+                    </Link>
+                </li>
+                <li>
+                    <Link to='https://github.com/anh-8922' target="_blank">
+                        <img className="social-icons" src={github1} alt="github"/>
+                    </Link>
+                </li>
+                <li>
+                    <Link to='mailto:anh-dev@hotmail.com' target="_blank"><img className="social-icons" src={hotmail} alt="hotmail"/>
+                    </Link>
+                </li>
+                <li>
+                    <Link to='https://www.linkedin.com/in/anh-kim-chau/' target="_blank">
+                        <img className="social-icons" src={linked} alt="linkedin"/>
+                    </Link>
+                </li>
             </ul>
             <ul className="nav-content">
                 <li><NavLink className='nav-items' to="/"><BsFillPersonLinesFill/><span>About me</span></NavLink></li>

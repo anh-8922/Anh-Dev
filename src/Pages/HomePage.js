56036bdec01531b26f2bdcf1d405236e8b75f43e
Anh Kim Chau <119595804+anh-8922@users.noreply.github.com>
2023-09-28 04:14:52 +0100

    update

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
     const navigate = useNavigate();
-    toMyProject = () => {
+    const toMyProject = () => {
         navigate('/portfolio');
     }
 
